0544f31dc88d93cf0b2230d194bb7383dc717dd3
williamleferrand <william@corefarm.com>
2010-08-04 23:59:45 +0200

    added hygears_database

---
+++ hygears_database/command.ml
@@ -0,0 +1,18 @@
+(*
+ * hyGears - database
+ *
+ * (c) 2010 Hypios SAS - William Le Ferrand william@hypios.com
+ *
+ *)
+
+
+let put_rdf connection namegraph rdf = 
+  Factory.put_rdf connection namegraph rdf
+    
+let put_turtle connection namegraph turtle = 
+  Factory.put_turtle connection namegraph turtle 
+  
+let exec_sparql connection query = 
+  Factory.exec_sparql connection query 
+
+   
