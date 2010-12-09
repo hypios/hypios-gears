c51f02590583159b2ba1fed102aef8447fd7ff79
Simon Marc <msimon@hypios.com>
2010-12-09 17:20:42 +0100

    remove useless hygears

--- hygears_database/types.ml
+++
@@ -1,1 +0,0 @@
-type result = Literal of string
--- hygears_doodle/misc.ml
+++
@@ -1,7 +0,0 @@
-let (>>>) f g = g f
-
-let debug fmt = 
-  Printf.ksprintf (fun s -> print_string s; flush stdout) fmt
-
-let display fmt = 
-  Printf.ksprintf (fun s -> print_string s; flush stdout) fmt
